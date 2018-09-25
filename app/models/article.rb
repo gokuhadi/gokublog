2f6de553863b3af61b422480a5efa2fd5f1814b8
salman hadi <salmanhadi1993@gmail.com>
2018-09-25 01:56:18 +0000

    models and validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  
-  
+  validates :title, presence: true, length: {minimum:3, maximum:50} #db will not allow empty titles in the db
+  validates :description, presence: true, length: {minimum:3, maximum:50}
   
 end
