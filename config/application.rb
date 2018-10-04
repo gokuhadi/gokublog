134898b953fe974de97b12d9cc91e6d4f25f9261
salman hadi <salmanhadi1993@gmail.com>
2018-10-04 12:27:49 +0000

    home,about

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,6 @@
 require File.expand_path('../boot', __FILE__)
+config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
 
 require 'rails/all'
 
